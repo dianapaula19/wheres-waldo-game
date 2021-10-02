a3825a1cd19bbf106fcf5be81560765d253af39d
dianapaula19 <dianabacircea19@gmail.com>
2021-10-03 00:19:29 +0300

    final version

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
-#include <GL/freeglut.h> // nu trebuie uitat freeglut.h (sau glut.h sau gl.h & glu.h)
-//#include <bits/stdc++.h>
+#include <GL/freeglut.h> 
 #include <iostream>
 #include <cmath>
 #define LEG_W 22
@@ -21,7 +20,6 @@
 using namespace std;
 
 const double TWO_PI = 6.2831853;
-GLuint clouds_list;
 float translate_clouds = 0.0;
 float alpha_clouds = 0.5;
 float rotate_wave_left = 0.0;
@@ -29,15 +27,17 @@ float alpha_wave_left = 0.1;
 float rotate_wave_right = 0.0;
 float alpha_wave_right = 0.1;
 float alpha_cloud = 0.1;
-float text_scale = 1.0f;
+float translate_person = 0.0;
+float scale_person = 0.0;
+float alpha_translate_person = 0.05;
+float alpha_scale_person = 0.001;
+float rotate_legs = 0.0;
+float alpha_legs = 0.1;
+float text_scale = 1.0f; 
+float text_apha = 0.001;
 bool found = 0;
 int lives = 3;
 
-
-class scrPt {
-	public:
-		GLint x, y;
-};
 
 struct RGB_color {
 	// the values for R, G and B go from 0 to 255
@@ -129,6 +129,91 @@ namespace Draw {
 		glEnd();
 
 	}
+
+	void move_person(Coord person, RGB_color shirt_color, RGB_color pants_color) {
+
+		glColor3f(pants_color.R, pants_color.G, pants_color.B);
+
+		translate_person += alpha_translate_person;
+		scale_person += alpha_scale_person;
+		rotate_legs += alpha_legs;
+
+		if (translate_person > 0) {
+			alpha_translate_person = -0.05;
+		}
+		else if (translate_person < -60) {
+			alpha_translate_person = 0.05;
+		}
+		if (scale_person > 1) {
+			alpha_scale_person = -0.001;
+		}
+		else if (scale_person < 0) {
+			alpha_scale_person = 0.001;
+		}
+		if (rotate_legs > 25) {
+			alpha_legs = -0.1;
+		}
+		else if (rotate_legs < -25) {
+			alpha_legs = 0.1;
+		}
+
+		// drawing the left leg
+		glPushMatrix();
+		glTranslated(person.x1 + ARM_W + ARM_SPACE + LEG_W + translate_person, person.y1 + LEG_H + translate_person, 0);
+		glRotated(rotate_legs, 0.0, 0.0, 1.0); // rotatia
+		glScaled(scale_person, scale_person, 1.0);
+		glTranslated(-(person.x1 + ARM_W + ARM_SPACE + LEG_W), -(person.y1 + LEG_H), 0);
+		glRecti(person.x1 + ARM_W + ARM_SPACE, person.y1, person.x1 + ARM_W + ARM_SPACE + LEG_W, person.y1 + LEG_H);
+		glPopMatrix();
+
+
+		// drawing the right leg
+		glPushMatrix();
+		glTranslated(person.x1 + ARM_W + ARM_SPACE + LEG_W + LEG_SPACE + translate_person, person.y1 + LEG_H + translate_person, 0);
+		glRotated(-rotate_legs, 0.0, 0.0, 1.0); // rotatia
+		glScaled(scale_person, scale_person, 1.0);
+		glTranslated(-(person.x1 + ARM_W + ARM_SPACE + LEG_W + LEG_SPACE), -(person.y1 + LEG_H), 0);
+		glRecti(person.x1 + ARM_W + ARM_SPACE + LEG_W + LEG_SPACE, person.y1, person.x1 + ARM_W + ARM_SPACE + LEG_W * 2 + LEG_SPACE, person.y1 + LEG_H);
+		glPopMatrix();
+
+		//the rest of the body
+		glPushMatrix();
+		glTranslated(person.x1 + ARM_W + ARM_SPACE + LEG_W + LEG_SPACE/2 + translate_person, person.y1 + LEG_H + translate_person, 0);
+		glScaled(scale_person, scale_person, 1.0);
+		glTranslated(- (person.x1 + ARM_W + ARM_SPACE + LEG_W + LEG_SPACE / 2), -(person.y1 + LEG_H), 0);
+
+		glColor3f(shirt_color.R, shirt_color.G, shirt_color.B);
+		// drawing the upper body
+		glRecti(person.x1 + ARM_W + ARM_SPACE, person.y1 + LEG_H, person.x1 + ARM_W + ARM_SPACE + LEG_W * 2 + LEG_SPACE, person.y1 + LEG_H + ARM_H);
+
+		// drawing the arms
+		glRecti(person.x1, person.y1 + LEG_H, person.x1 + ARM_W, person.y1 + LEG_H + ARM_H); // left arm
+		glRecti(person.x1 + ARM_W + ARM_SPACE * 2 + LEG_W * 2 + LEG_SPACE, person.y1 + LEG_H, person.x1 + ARM_W * 2 + ARM_SPACE * 2 + LEG_W * 2 + LEG_SPACE, person.y1 + LEG_H + ARM_H); // right arm
+
+		// drawing the head
+
+		glColor3f(Colors::FACE.R, Colors::FACE.G, Colors::FACE.B);
+		glRecti(person.x1 + ARM_W + ARM_SPACE + HEAD_SPACE_W, person.y1 + LEG_H + ARM_H + HEAD_SPACE_H, person.x1 + ARM_W + ARM_SPACE + HEAD_SPACE_W + HEAD, person.y1 + LEG_H + ARM_H + HEAD_SPACE_H + HEAD);
+
+		if (alpha_scale_person > 0)
+		{
+			// drawing the mask
+			glColor3f(Colors::LIGHT_BLUE_TWO.R, Colors::LIGHT_BLUE_TWO.G, Colors::LIGHT_BLUE_TWO.B);
+			glRecti(person.x1 + ARM_W + ARM_SPACE + HEAD_SPACE_W + 4, person.y1 + LEG_H + ARM_H + HEAD_SPACE_H + 4, person.x1 + ARM_W + ARM_SPACE + HEAD_SPACE_W + MASK_W + 4, person.y1 + LEG_H + ARM_H + HEAD_SPACE_H + MASK_H + 4);
+
+			glBegin(GL_LINES);
+
+			glLineWidth(6);
+			glColor3f(Colors::WHITE.R, Colors::WHITE.G, Colors::WHITE.B);
+			glVertex2f(person.x1 + ARM_W + ARM_SPACE + HEAD_SPACE_W, person.y1 + LEG_H + ARM_H + HEAD_SPACE_H + 4);
+			glVertex2f(person.x1 + ARM_W + ARM_SPACE + HEAD_SPACE_W + MASK_W + 8, person.y1 + LEG_H + ARM_H + HEAD_SPACE_H + 4);
+			glVertex2f(person.x1 + ARM_W + ARM_SPACE + HEAD_SPACE_W, person.y1 + LEG_H + ARM_H + HEAD_SPACE_H + MASK_H + 4);
+			glVertex2f(person.x1 + ARM_W + ARM_SPACE + HEAD_SPACE_W + MASK_W + 8, person.y1 + LEG_H + ARM_H + HEAD_SPACE_H + MASK_H + 4);
+			glEnd();
+		}
+
+		glPopMatrix();
+	}
 	void wave_left(Coord person, RGB_color shirt_color) {
 		glColor3f(shirt_color.R, shirt_color.G, shirt_color.B);
 		glPushMatrix();
@@ -179,23 +264,25 @@ namespace Draw {
 		}
 	}
 
-	void print_increasing_text(Point P, std::string text, int size) {
-		
+	void print_increasing_text(Point P, std::string text, int size) 
+	{
+		if (text_scale <= 1)
+			text_apha = 0.001;
+		else if (text_scale >= 1.5)
+			text_apha = -0.001;
+
 		glColor3f(Colors::RED.R, Colors::RED.G, Colors::RED.B);
-		
 		glPushMatrix();
-		
-		glScalef(text_scale, text_scale, text_scale);
-		text_scale += 0.1;
+		text_scale += text_apha;
+		glTranslated(P.x + 200, P.y, 0);
+		glScalef(text_scale, text_scale, 1.0);
+		glTranslated(-P.x - 200, - P.y, 0);
 
 		for (int i = 0; i < text.size(); i++) {
-			glRasterPos2i(P.x + i * size, P.y);
+			glRasterPos2i(P.x + i * (size), P.y);
 			glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, text[i]);
 		}
-
 		glPopMatrix();
-		
-
 	}
 
 	void cloud(Coord cloud) {
@@ -241,7 +328,6 @@ namespace Draw {
 		glPushMatrix();
 		glTranslatef(alpha_cloud, 0.0, 0.0);
 		alpha_cloud += speed;
-		std::cout << alpha_cloud << "\n";
 		Draw::cloud(cloud);
 		glPopMatrix();
 		if (alpha_cloud > 1300) {
@@ -257,73 +343,57 @@ namespace Draw {
 			glRecti(person.x1 + ARM_W + ARM_SPACE, person.y1 + LEG_H + i * 24, person.x1 + ARM_W + ARM_SPACE + 2 * LEG_W + LEG_SPACE, person.y1 + LEG_H + 12 + i * 24);
 		}
 	}
-
-	/*
-
-	void create_circle(GLuint clouds_list, double x, double y) {
-		scrPt hexVertex;
-		GLdouble hexTheta;
-		GLint k;
-		clouds_list = glGenLists(1);
-		glNewList(clouds_list, GL_COMPILE);
-		glColor3f(Colors::WHITE.R, Colors::WHITE.G, Colors::WHITE.B);
+	void heart(Point P)
+	{
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		glColor3f(Colors::RED.R, Colors::RED.G, Colors::RED.B);
 		glBegin(GL_POLYGON);
-		for (k = 0; k < 100; k++)
-		{
-			hexTheta = TWO_PI * k / 100;
-			hexVertex.x = x + y * cos(hexTheta);
-			hexVertex.y = x + y * sin(hexTheta);
-			glVertex2i(hexVertex.x, hexVertex.y);
-		}
+		glVertex2s(P.x + 20, P.y);
+		glVertex2s(P.x + 28, P.y + 12);
+		glVertex2s(P.x + 32, P.y + 20);
+		glVertex2s(P.x + 32, P.y + 24);
+		glVertex2s(P.x + 28, P.y + 28);
+		glVertex2s(P.x + 24, P.y + 28);
+		glVertex2s(P.x + 20, P.y + 24);
+		glVertex2s(P.x + 16, P.y + 28);
+		glVertex2s(P.x + 12, P.y + 28);
+		glVertex2s(P.x + 8, P.y + 24);
+		glVertex2s(P.x + 8, P.y + 20);
+		glVertex2s(P.x + 12, P.y + 12);
 		glEnd();
-		glEndList();
 	}
-
-
-	void clouds() {
-		glPushMatrix();
-		glTranslated(200, CLOUDS_COORD.y1, 0.0);
-		glCallList(clouds_list);
-		glColor3f(Colors::WHITE.R, Colors::WHITE.G, Colors::WHITE.B);
-		glPopMatrix();
-
-		/*glPushMatrix();
-		glTranslated(CLOUDS_COORD.x1 + 100 + translate_clouds, CLOUDS_COORD.y1, 0.0);
-		glCallList(clouds_list[1]);
-		glPopMatrix();
-
-		glPushMatrix();
-		glTranslated(CLOUDS_COORD.x1 + 200 + translate_clouds, CLOUDS_COORD.y1, 0.0);
-		glCallList(clouds_list[2]);
-		glPopMatrix();
-		
-		translate_clouds = translate_clouds + alpha_clouds;
-		glutSwapBuffers();
-		if (translate_clouds > 1200.0)
-			translate_clouds = 0.0;
+	void draw_hearts()
+	{
+		if (lives == 1)
+			heart(Point(50, 520));
+		else if (lives == 2)
+		{
+			heart(Point(50, 520));
+			heart(Point(90, 520));
+		}
+		else if (lives == 3)
+		{
+			heart(Point(50, 520));
+			heart(Point(90, 520));
+			heart(Point(130, 520));
+		}
 	}
-	*/
-
 }
 
 void init (void) {
 	glClearColor(Colors::SKY.R, Colors::SKY.G, Colors::SKY.B, 0.0); // precizeaza culoarea de fond a ferestrei de vizualizare
     glMatrixMode(GL_PROJECTION);  // se precizeaza este vorba de o reprezentare 2D, realizata prin proiectie ortogonala
-	gluOrtho2D(0.0, 1200.0, 0.0, 900.0); // sunt indicate coordonatele extreme ale ferestrei de vizualizare
+	gluOrtho2D(0.0, 1200.0, 0.0, 800.0); // sunt indicate coordonatele extreme ale ferestrei de vizualizare
 
-
-	//Draw::create_circle(clouds_list, CLOUDS_COORD.x1, CLOUDS_COORD.y1);
-	//Draw::create_circle(clouds_list[1], CLOUDS_COORD.x1 + 100, CLOUDS_COORD.y1);
-	//Draw::create_circle(clouds_list[2], CLOUDS_COORD.x1 + 200, CLOUDS_COORD.y1);
 }
 
 void draw() {
 
 	Draw::grass();
-	Draw::move_cloud(Coord(-104, 750, 0, 0), 0.2);
-	Draw::print_text(Point(250, 800), "WHERE'S WALLY?", Colors::RED, 50);
+	Draw::move_cloud(Coord(-104, 620, 0, 0), 0.2);
+	Draw::print_text(Point(250, 750), "WHERE'S WALLY?", Colors::RED, 50);
 	Draw::print_text(Point(250, 700), "Social Distancing Edition", Colors::BLACK, 30);
-	Draw::person(Coord(150, 200, 0, 0), Colors::YELLOW, Colors::PURPLE);
+	Draw::move_person(Coord(150, 200, 0, 0), Colors::YELLOW, Colors::PURPLE);
 	Draw::person(Coord(350, 200, 0, 0), Colors::BLUE, Colors::DARK_BLUE);
 	Draw::person(Coord(550, 200, 0, 0), Colors::LIGHT_BLUE_ONE, Colors::DARK_BLUE);
 	Draw::wave_left(Coord(550, 200, 0, 0), Colors::LIGHT_BLUE_ONE);
@@ -331,7 +401,7 @@ void draw() {
 	Draw::stripes(Coord(Waldo_x, Waldo_y, 0, 0));
 	Draw::person(Coord(950, 200, 0, 0), Colors::LIGHT_BLUE_TWO, Colors::DARK_BLUE);
 	Draw::wave_right(Coord(950, 200, 0, 0), Colors::LIGHT_BLUE_TWO);
-
+	Draw::draw_hearts();
 }
 
 void found_waldo() {
@@ -343,37 +413,29 @@ void found_waldo() {
 	glVertex2i(Waldo_x + ARM_W * 2 + ARM_SPACE * 2 + LEG_W * 2 + LEG_SPACE + 10, Waldo_y + LEG_H + ARM_H + HEAD_SPACE_H + HEAD + 10);
 	glVertex2i(Waldo_x - 10, Waldo_y + LEG_H + ARM_H + HEAD_SPACE_H + HEAD + 10);
    	glEnd();
-	Draw::print_text(Point(250, 650), "You found Waldo :)", Colors::RED, 30);
-	Draw::print_text(Point(250, 600), "Right click to reset the game", Colors::BLACK, 20);
+	Draw::print_increasing_text(Point(250, 600), "You found Waldo :)", 30);
+	Draw::print_text(Point(250, 570), "Right click to reset the game", Colors::BLACK, 20);
 
 }
+void lost_game() {
 
+	Draw::print_increasing_text(Point(250, 600), "You lost :(", 30);
+	Draw::print_text(Point(250, 570), "Right click to reset the game", Colors::BLACK, 20);
+}
 void desen (void) {
 
 	glClear(GL_COLOR_BUFFER_BIT);
-	if (found == 1) {
-		draw();
+	draw();
+	if (lives <= 0)
+		lost_game();
+	else if (found == 1)
 		found_waldo();
-	} else {
-		draw();
-	}
 
 	glFlush();
 	glutPostRedisplay();
-
 }
-/*
-void ending(void) {
-	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3f(Colors::RED.R, Colors::RED.G, Colors::RED.B);
-	glRecti(Waldo_x, Waldo_y, Waldo_x + ARM_W * 2 + ARM_SPACE * 2 + LEG_W * 2 + LEG_SPACE, Waldo_y + LEG_H + ARM_H + HEAD_SPACE_H + HEAD); // highlight Waldo
-	glutPostRedisplay();
-}
-*/
 
 void mouse(int button, int state, int x, int y) {
-	cout << x << " " <<  y << endl;
-	cout << Waldo_x + ARM_W * 2 + ARM_SPACE * 2 + LEG_W * 2 + LEG_SPACE << " " << Waldo_y + LEG_H + ARM_H + HEAD_SPACE_H + HEAD << endl;
 	bool condition = (x >= Waldo_x && x <= Waldo_x + ARM_W * 2 + ARM_SPACE * 2 + LEG_W * 2 + LEG_SPACE) && (WINDOW_H - y >= Waldo_y && y <= WINDOW_H - Waldo_y + LEG_H + ARM_H + HEAD_SPACE_H + HEAD);
 	switch (button) {
 	case GLUT_LEFT_BUTTON:
@@ -381,11 +443,18 @@ void mouse(int button, int state, int x, int y) {
 			if (condition) {
 				found = 1;
 				cout << "DA" << endl;
+				cout << x << " " << y << endl;
 			}
+			else
+				lives--;
 		break;
 	case GLUT_RIGHT_BUTTON:
-		if (state == GLUT_DOWN)
+		if (state == GLUT_DOWN && (found == 1 || lives <= 0))
+		{
 			found = 0;
+			lives = 3;
+		}
+
 		break;
 	default:
 		break;
@@ -397,7 +466,7 @@ int main (int argc, char** argv) {
 	glutInit (&argc, argv); // initializare GLUT
 	glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB); // se utilizeaza un singur buffer | modul de colorare RedGreenBlue (= default)
 	glutInitWindowPosition (100, 100); // pozitia initiala a ferestrei de vizualizare (in coordonate ecran)
-	glutInitWindowSize(1200, 900); // dimensiunile ferestrei 
+	glutInitWindowSize(1200, 800); // dimensiunile ferestrei 
 	glutCreateWindow("Where's Wally: Social Distancing Edition"); // creeaza fereastra, indicand numele ferestrei de vizualizare - apare in partea superioara
 
 	init(); // executa procedura de initializare
